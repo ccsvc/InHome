7419c88e04814e0785540e25cab910810a11b906
Walter Wms <ccsvc2019@gmail.com>
2025-12-23 09:28:29 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple enhancement: smooth scroll for internal navigation if added later
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
